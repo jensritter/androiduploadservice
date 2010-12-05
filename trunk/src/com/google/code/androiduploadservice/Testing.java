c6aa2250bcda0e469219a66bbc35dc45491e9fd4
jens.ritter <jens.ritter@894a1bba-0015-1cf9-6558-6d0641f0c328>
2010-12-05 15:42:28 +0000

    Basic Project

---
+++ trunk/src/com/google/code/androiduploadservice/Testing.java
@@ -0,0 +1,15 @@
+package com.google.code.androiduploadservice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Testing extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
